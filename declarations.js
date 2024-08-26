4383f5734e648f9861c5678311624d816914ff8b
Philip <oumaphilip01@gmail.com>
2024-08-26 13:36:36 +0300

    correct object to Object

--- declarations.js
+++ declarations.js
@@ -1,14 +1,14 @@
 const escapeStr = "`, \, /, \" ' ";
-const arr = object.freeze([4, '2']);
-const obj = object.freeze({
+const arr = Object.freeze([4, '2']);
+const obj = Object.freeze({
     str: 'a string',
     num: 67,
     bool: false,
     undef: undefined
 })
 const nested = object.freeze({
-    arr: object.freeze([4, undefined, '2']),
-    obj: object.freeze({
+    arr: Object.freeze([4, undefined, '2']),
+    obj: Object.freeze({
         str:'string',
         num: 89,
         bool: true
