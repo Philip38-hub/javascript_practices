c064c0230f52fb5478bc3cbd06ca9b6dadd4043b
Philip <oumaphilip01@gmil.com>
2024-08-26 16:28:19 +0300

    add change.js

---
+++ change.js
@@ -0,0 +1,11 @@
+const sourceObject = {
+    num: 42,
+    bool: true,
+    str: 'some text',
+    log: console.log,
+  }
+  
+var get = (key) => sourceObject[key];
+var set = (key, value) => {(sourceObject[key] = value); return sourceObject[key];}
+console.log(get('num'));
+console.log(set('num', 43));
