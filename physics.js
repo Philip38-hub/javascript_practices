1f51839159b97542fb97c93a5159c5c4fe743726
Philip <oumaphilip01@gmil.com>
2024-08-27 15:32:19 +0300

    ammend physics.js to check for type undefined

--- physics.js
+++ physics.js
@@ -1,9 +1,9 @@
 var getacceleration = (obj) => {
-   if (typeof obj.f === 'number' && typeof obj.m === 'number' || obj.m !== 0) {
+   if (obj.f !== undefined && typeof obj.m !== undefined || obj.m !== 0) {
     return obj.f/obj.m;
-   } else if (typeof obj.Δv === 'number' && typeof obj.Δt === 'number' || obj.t !== 0) {
+   } else if (typeof obj.Δv !== undefined && typeof obj.Δt !== undefined || obj.t !== 0) {
     return obj.Δv/obj.Δt;
-   } else if (typeof obj.t === 'number' && typeof obj.d === 'number' || obj.d !== 0) {
+   } else if (typeof obj.t !== undefined && typeof obj.d !== undefined || obj.d !== 0) {
     return obj.d*obj.d/obj.t*obj.t;
    } else {
     return 'impossible'
