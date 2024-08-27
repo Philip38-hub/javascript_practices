64ddde08c0d7d3c2214da5d8d5b60ac9a65bf003
Philip <oumaphilip01@gmil.com>
2024-08-27 10:24:38 +0300

    capitalize takes even all capitalized string letters

--- method-man.js
+++ method-man.js
@@ -2,4 +2,12 @@ var words = (str) => str.split(' ');
 var sentence = (arr) => arr.join(' ');
 var yell = (str) => str.toUpperCase();
 var whisper = (str) => '*'+str.toLowerCase()+'*';
-var capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+var capitalize = (str) => {
+    if (str[0] !== str[0].toUpperCase()) {
+        return str[0].toUpperCase() + str.slice(1);
+    } else {
+        return str[0] + str.slice(1).toLowerCase();
+    }
+};
+
+console.log(capitalize('STR'));
