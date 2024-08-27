a3d4d09d77f82b1f03d4e35b0bc3c1a12c4ecae0
Philip <oumaphilip01@gmil.com>
2024-08-27 10:14:05 +0300

    add method-man.js

---
+++ method-man.js
@@ -0,0 +1,5 @@
+var words = (str) => str.split(' ');
+var sentence = (arr) => arr.join(' ');
+var yell = (str) => str.toUpperCase();
+var whisper = (str) => '*'+str.toLowerCase()+'*';
+var capitalize = (str) => str[0].toUpperCase() + str.slice(1);
