0d939e29d88fb5fb8ef30d254a25d9ca7da269fa
Philip <oumaphilip01@gmil.com>
2024-08-28 16:38:17 +0300

    add keep-cut.js

--- keep-cut.js
+++ keep-cut.js
@@ -3,7 +3,11 @@ var cutLast = (str) => str.slice(0, -2);
 var cutFirstLast = (str) => cutFirst(cutLast(str));
 var keepFirst = (str) => str.slice(0, 2);
 var keepLast = (str) => str.slice(-2);
-var keepFirstLast = (str) => keepFirst(str) + keepLast(str);
+var keepFirstLast = (str) => str.length <= 4 ? str : keepFirst(str) + keepLast(str);
 
-console.log(cutFirstLast('james'))
-console.log(keepFirstLast('james'))
+// console.log(cutFirst("Hello")); // Output: "llo"
+// console.log(cutLast("Hello")); // Output: "Hel"
+// console.log(cutFirstLast("Hello")); // Output: "l"
+// console.log(keepFirst("Hello")); // Output: "He"
+// console.log(keepLast("Hello")); // Output: "lo"
+// console.log(keepFirstLast("Hello")); // Output: "Helo"
