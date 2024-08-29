c7b283dc6abd1a7dc06e13d54e34bcb20519ea33
Philip <oumaphilip01@gmil.com>
2024-08-29 16:41:51 +0300

    add reverser.js

--- reverser.js
+++ reverser.js
@@ -3,10 +3,14 @@ const reverse = (arr) => {
    for (let i = arr.length - 1; i >=0; i--) {
         newArr.push(arr[i]);
    }
+   if (typeof arr === 'string') {
+    return newArr.join('')
+   }
    return newArr
 }
 
-// const originalArray = [1, 2, 3, 4];
-// const reversedArray = reverse(originalArray);
+const originalArray = [1, 2, 3, 4];
+const reversedArray = reverse(originalArray);
 
-// console.log(reversedArray); // Output: [4, 3, 2, 1]
+console.log(reversedArray); // Output: [4, 3, 2, 1]
+console.log(reverse('pouet'))
