20948aefbd217fc95182340e596caccfcb3a60e2
Philip <oumaphilip01@gmil.com>
2024-08-29 17:31:40 +0300

    add unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -1,3 +1,38 @@
+// const split = (str, sep) => {
+//     let result = [];
+//     let current = '';
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (sep.length > 1) {
+//             // Correctly compare the substring of str with sep
+//             if (str.substr(i, sep.length) === sep) {
+//                 if (current !== '') {
+//                     result.push(current);
+//                     current = '';
+//                 }
+//                 i += sep.length - 1; // Move the index forward by the length of the separator
+//             } else {
+//                 current += str[i];
+//             }
+//         } else {
+//             if (str[i] === sep) {
+//                 if (current !== '') {
+//                     result.push(current);
+//                     current = '';
+//                 }
+//             } else {
+//                 current += str[i];
+//             }
+//         }
+//     }
+
+//     if (current !== '') {
+//         result.push(current);
+//     }
+
+//     return result;
+// }
+
 const split = (str, sep) => {
     let result = [];
     let current = '';
@@ -6,34 +41,26 @@ const split = (str, sep) => {
         if (sep.length > 1) {
             // Correctly compare the substring of str with sep
             if (str.substr(i, sep.length) === sep) {
-                if (current !== '') {
-                    result.push(current);
-                    current = '';
-                }
+                result.push(current);
+                current = '';
                 i += sep.length - 1; // Move the index forward by the length of the separator
             } else {
                 current += str[i];
             }
         } else {
             if (str[i] === sep) {
-                if (current !== '') {
-                    result.push(current);
-                    current = '';
-                }
+                result.push(current);
+                current = '';
             } else {
                 current += str[i];
             }
         }
     }
-
-    if (current !== '') {
-        result.push(current);
-    }
+    // After the loop, push the final part
+    result.push(current);
 
     return result;
 }
-
-
 const join = (arr, sep) => {
     let result = '';
     for (let i = 0; i < arr.length; i++) {
@@ -45,4 +72,6 @@ const join = (arr, sep) => {
     return result
 }
 
-console.log(split('ggg - ddd - b', ' - '))
+// console.log(split('ee,ff,g,', ','))
+// console.log(split('rrrr', 'rr'))
+// console.log(split('ggg - ddd - b', ' - '))
