8218de02ab7ea5dd815ffdfaeeff4e90c1fd3ade
Philip <oumaphilip01@gmil.com>
2024-08-29 18:22:52 +0300

    add molecules-cells.js

---
+++ molecules-cells.js
@@ -0,0 +1,34 @@
+const rna = (str) =>{
+    str.toUpperCase();
+    var result = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'G') {
+            result += 'C';
+        } else if (str[i] === 'C') {
+            result += 'G';
+        } else if (str[i] === 'T') {
+            result += 'A';
+        } else if (str[i] === 'A') {
+            result += 'U';
+        }
+    }
+    return result
+    
+}
+
+const dna = (str) => {
+    str.toUpperCase();
+    var result = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'C') {
+            result += 'G';
+        } else if (str[i] === 'G') {
+            result += 'C';
+        } else if (str[i] === 'A') {
+            result += 'T';
+        } else if (str[i] === 'U') {
+            result += 'A';
+        }
+    }
+    return result
+}
