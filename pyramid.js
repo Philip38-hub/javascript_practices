eb62931fc4bc18d6fb8e042ce85cd51eb3bb4a72
Philip <oumaphilip01@gmil.com>
2024-08-30 16:40:35 +0300

    add pyramid.js

---
+++ pyramid.js
@@ -0,0 +1,18 @@
+const pyramid = (str, num) => {
+    var result = '';
+    for (let i = 1; i <= num; i++) {
+        for (let j = num-i; j >= 0; j--) {
+            result += ' '.repeat(str.length)
+        }
+       let k = 2*i-1
+        for (let j = 0; j < k; j++) {
+            result += str
+        }
+        if (i !== num) {
+            result += '\n'
+        }
+    }
+    return result
+}
+
+// console.log(pyramid('pk', 5))
