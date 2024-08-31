b12d4e65c11bd3f33dcd6f863fb8644a768652a4
Philip <oumaphilip01@gmil.com>
2024-08-31 13:44:26 +0300

    add ion-out.js

---
+++ ion-out.js
@@ -0,0 +1,13 @@
+var ionOut = (str) => {
+    const words = str.split(', ');
+    const result = [];
+    words.forEach(word => {
+        const match = /(?<=t)ion/.test(word);
+        if (match) {
+            result.push(word.slice(0, word.length - 3));
+        } 
+    })
+    return result
+}
+
+// console.log(ionOut('transfusion'))
