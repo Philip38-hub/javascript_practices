7704eed44db8ab8fe7905895d6c1b6acd54daafb
Philip <oumaphilip01@gmil.com>
2024-09-03 14:57:34 +0300

    add get sometime

---
+++ get-some-time.js
@@ -0,0 +1,30 @@
+function firstDayWeek(week, year) {
+    let time = new Date(year);
+    if (week === 1) {
+        time.setHours(24);
+        return formattedDate(time);
+    }
+    
+    let dayPlus = week * 7 * 24;
+    time.setHours(dayPlus - 123);
+    
+    for (let i = 0; i < 7; i++) {
+        if (getWeekDay(time) === 'Monday') {
+            return formattedDate(time);
+        }
+        time.setHours(-24);
+    }
+    return time;
+}
+
+function getWeekDay(date) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDay() - 1];
+}
+
+function formattedDate(d) {
+    let month = String(d.getMonth() + 1).padStart(2, '0');
+    let day = String(d.getDate() - 1).padStart(2, '0');
+    let year = String(d.getFullYear()).padStart(4, '0');
+    return `${day}-${month}-${year}`;
+}
