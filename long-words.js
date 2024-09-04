404180beaf72c13c96420b24bcb1c7e63fb4b76b
Philip <oumaphilip01@gmil.com>
2024-09-04 15:12:35 +0300

    add long-words.js

--- long-words.js
+++ long-words.js
@@ -1,25 +1,18 @@
-const adder = (numbers, initial = 0) =>{
-    return numbers.reduce((sum, num) => sum+num, initial);
-}
-
-const sumOrMul = (numbers, initial = 0) =>{
-    return numbers.reduce((result, num) => {
-        return num %2 === 0 ? result*num : result + num;
-    }, initial);
-}
-
-const funcExec = (functions, initial = null) => {
-    return functions.reduce((result, func) => {
-        return func(result);
-    }, initial);
+const longWords = (arr) => {
+    return arr.every(element => typeof element === 'string' &&
+        element.length >= 5);
 };
-// console.log(adder([1, 2, 3, 4])); 
 
-// console.log(sumOrMul([1, 2, 3, 5, 8], 5)); 
+const oneLongWord = (arr) => {
+    return arr.some(element => typeof element === 'string' &&
+        element.length >= 10);
+};
 
-// const functions = [
-//     (x) => adder([1, 2, 3], x),
-//     (x) => sumOrMul([2, 4, 6], x)
-// ];
+const noLongWords = (arr) => {
+    return !arr.some(element => typeof element === 'string' &&
+        element.length >=7);
+};
 
-// console.log(funcExec(functions, 0));
+// console.log(longWords(["apple", "banana", "cherry"])); 
+// console.log(oneLongWord(["tiny", "longerString", "anotherOne"]));
+// console.log(noLongWords(["short", "tiny", "big", "longer"]));
