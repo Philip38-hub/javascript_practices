1e3af76f96b17e498bd9b183fb8a42e0d1e05631
Philip <oumaphilip01@gmil.com>
2024-09-04 14:36:40 +0300

    add using-filter.js

---
+++ using-filter.js
@@ -0,0 +1,40 @@
+const filterShortStateName = (arr) => arr.filter((str) => str.length < 7);
+const filterStartVowel = (arr) => arr.filter((str) => str.test(/^[aeiou]/i));
+const filter5Vowels = (arr) => {
+    return arr.filter(str => {
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        let vowelCount = 0;
+        for (char in str) {
+            if (vowels.includes(char)) {
+                vowelCount++
+            }
+        }
+        return vowelCount>=5;
+    })
+}
+
+function filter1DistinctVowel(strs) {
+    return strs.filter(str => {
+      const vowels = ['a', 'e', 'i', 'o', 'u'];
+      const vowelSet = new Set();
+  
+      for (let char of str) {
+        if (vowels.includes(char)) {
+          vowelSet.add(char);
+        }
+      }
+  
+      return vowelSet.size === 1;
+    });
+  }
+
+  function multiFilter(objects) {
+    return objects.filter(obj => {
+      return (
+        obj.capital.length >= 8 &&
+        !['a', 'e', 'i', 'o', 'u'].includes(obj.name[0]) &&
+        ['a', 'e', 'i', 'o', 'u'].some(vowel => obj.tag.includes(vowel)) &&
+        obj.region !== "South"
+      );
+    });
+  }
