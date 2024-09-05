d7d0e15be2cb3a1ec455733984b0f52b72e5df8d
Philip <oumaphilip01@gmil.com>
2024-09-05 13:38:19 +0300

    add has city.js

---
+++ has-city.js
@@ -0,0 +1,9 @@
+const hasCity = (country, cities) => {
+    return (city) => cities.includes(city)? `${city} is a city from ${country}` :
+         `${city} is not a city from ${country}`;
+}
+
+// const isKenyaCity = hasCity('Kenya', ['Kisumu', 'Nairobi', 'Mombasa']);
+
+// console.log(isKenyaCity('Kisumu'));    
+// console.log(isKenyaCity('London'));    
