9f6809ece50e087cbae3535b9131a19ca907dd6d
Philip <oumaphilip01@gmil.com>
2024-09-07 14:58:19 +0300

    add keycodes-symphony.js

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -15,18 +15,39 @@ export const compose = () => {
       }
       // Check if the pressed key is part of the lowercase alphabet
       else {
-        // Create a new div element for the note
-        const note = document.createElement('div');
-        note.classList.add('note');  // Adding the 'note' class for styling
-        note.textContent = event.key; // Display the pressed key inside the div
-        
-        // Generate a random color for each note
-        note.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-        
-        // Append the new note to the notes container
-        notesContainer.appendChild(note);
-  
+         // Create a new div element for the note
+      const note = document.createElement('div');
+      note.classList.add('note');  // Adding the 'note' class for styling
+      note.textContent = event.key; // Display the pressed key inside the div
+      
+      // Generate a unique color for each note
+      const color = generateUniqueColor();
+      note.style.backgroundColor = color;
+      
+      // Append the new note to the notes container
+      notesContainer.appendChild(note);
+
       } 
     });
   };
   
+ export const generateUniqueColor = () => {
+    let color;
+    do {
+      color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    } while (isColorDuplicate(color));
+    return color;
+  };
+  
+  // Set to keep track of used colors
+  const usedColors = new Set();
+  
+  // Check if the color has been used before
+  const isColorDuplicate = (color) => {
+    if (usedColors.has(color)) {
+      return true;
+    } else {
+      usedColors.add(color);
+      return false;
+    }
+  };
