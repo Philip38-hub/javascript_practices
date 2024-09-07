8fd84cc4a73b55b953680678597c952b9247b768
Philip38-hub <80720299+Philip38-hub@users.noreply.github.com>
2024-09-07 10:43:21 +0300

    add harder-bigger-bolder-stronger.js

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,23 @@
+export const generateLetters = () => {
+    let body = document.querySelector('body')
+    for (let count = 1; count <= 120; count++) {
+        let letters = document.createElement('div');
+        letters.textContent = createRandomString();
+        letters.style.fontSize = `${10 + count}px`;
+        if (count <= 40) {
+            letters.style.fontWeight = '300';
+        } else if (count > 40 && count <= 80) {
+            letters.style.fontWeight = '400';
+        } else {
+            letters.style.fontWeight = '600';
+        }
+        body.appendChild(letters);
+    }
+}
+function createRandomString() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    result = chars.charAt(Math.floor(Math.random() * chars.length));
+    return result;
+}
+
