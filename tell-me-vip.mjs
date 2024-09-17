f5295fb1c2baa7adbd8fa67bdb82789ce8af48c9
Philip <oumaphilip01@gmil.com>
2024-09-17 15:48:48 +0300

    update tell-me-vip.mjs

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,16 +1,31 @@
+import { writeFile } from 'node:fs/promises';
+import { readFile } from 'node:fs/promises';
 import { readdir } from 'node:fs/promises';
 import { argv } from 'node:process'
 
-let filePath=argv[2]
+let filePath = argv[2]
 let files
+let store = []
+let list = ""
 try {
     files = await readdir(filePath);
-    let result = split(files)
-    result.sort()
-  //  console.log(result)
-    for (let i = 0; i < result.length; i++) {
-        console.log(`${i+1}. ${result[i]}`)
+    for (let i = 0; i < files.length; i++) {
+        let content = await readFile(`${filePath}/${files[i]}`, 'utf8')
+        let obj = (JSON.parse(content))
+        if (obj.answer === 'yes') {
+            store.push(files[i])
+        }
     }
+    let result = split(store)
+    result.sort()
+    //  console.log(result)
+    for (let i = 0; i < result.length; i++) {
+        list += `${i + 1}. ${result[i]}`;
+        if (i < result.length - 1) {
+            list += "\n";
+        }
+    }
+    writeFile('vip.txt', list)
 } catch (err) {
     console.error(err);
 }
@@ -21,9 +36,8 @@ function split(arr) {
     for (let file of arr) {
         let store = file.split('_')
         store[1] = store[1].slice(0, -5)
-        let conc = store[1] + " " + store[0]        
-        result.push(conc) 
-        
+        let conc = store[1] + " " + store[0]
+        result.push(conc)
     }
     return result
 }
